6bd645894b5da1bc80c194323c4677c71d54ed4f
ayoub chakri <ayoub-chakri@hotmail.com>
2019-04-20 11:30:11 +0100

    frontend routing & auth module

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { AppRoutingModule } from './routes/app.routing.module';
+import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component'
+import { AuthModule } from './auth/auth.module';
+import { EmptyComponentComponent } from './pages/empty-component/empty-component.component'
 
 @NgModule({
 
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    EmptyComponentComponent
   ],
 
   imports: [
     BrowserModule,
+    AuthModule,
     NgbModule,
     AppRoutingModule
   ],
--- front/src/app/auth/auth.module.ts
+++ front/src/app/auth/auth.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [LoginComponent],
+
   imports: [
     CommonModule
+  ],
+  
+  declarations: [
+    LoginComponent, 
+    RegisterComponent
+  ],
+
+  exports : [
+    LoginComponent, 
+    RegisterComponent
   ]
+
 })
 export class AuthModule { }
