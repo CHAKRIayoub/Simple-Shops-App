4886b7c77a9324020a5bac84608a30c3858a4ddb
ayoub chakri <ayoub-chakri@hotmail.com>
2019-04-21 14:52:10 +0100

    adding backend Models & shops route, controller

--- back/routes/api.php
+++ back/routes/api.php
@@ -8,7 +8,11 @@ Route::group(['middleware'=>'api'], function($router){
 	Route::post('login', 'AuthController@login');
 	Route::post('signup', 'AuthController@signup');
 	Route::post('logout', 'AuthController@logout');
+
 	Route::post('refresh', 'AuthController@refresh');
 	Route::post('me', 'AuthController@me');
+
+	Route::resource('shops','ShopController');
+	
 
 });
