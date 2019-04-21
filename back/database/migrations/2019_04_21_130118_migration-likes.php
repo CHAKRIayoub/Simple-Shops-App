25097111d0dc45f04556415c19338e27ddb6c3fd
ayoub chakri <ayoub-chakri@hotmail.com>
2019-04-21 14:22:15 +0100

    add some migrations

--- back/database/migrations/2019_04_21_130118_migration-likes.php
+++ back/database/migrations/2019_04_21_130118_migration-likes.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class MigrationLikes extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('likes', function (Blueprint $table) {
+            
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email',35)->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->integer('user_id');
+            $table->integer('shop_id');
+
         });
     }
 
@@ -31,6 +29,7 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users'); 
+        Schema::dropIfExists('shops'); 
+
     }
 }
