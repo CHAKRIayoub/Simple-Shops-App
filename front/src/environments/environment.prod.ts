7f38a54d5d7c76fb84f159577eddd7401299f083
ayoub chakri <ayoub.chakri.dev@gmail.com>
2020-11-03 22:39:04 +0100

    aut init

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api: 'http://localhost:1/api',
-  images_link : 'http://localhost:1/images'
+  api: 'http://localhost:8000/api',
+  images_link : 'http://localhost:8000/images'
 };
